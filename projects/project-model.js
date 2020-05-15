c2088eee9a9d52b11c46d66394bde04a67a85ec2
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-05-15 11:32:34 -0500

    done! with stretch

--- projects/project-model.js
+++ projects/project-model.js
@@ -2,25 +2,61 @@ const db = require('../data/db-config')
 
 module.exports = {
     getProjects,
+    getProjectById,
+    insertProject,
     getResources,
+    getResourceById,
+    insertResource,
     getTasks,
+    insertTask,
 }
 
 function getProjects(){
     return db('projects')
 }
 
+function getProjectById(id) {
+    return db('users')
+      .where({ id })
+      .first()
+  }
+
+function insertProject(project){
+    return db('projects')
+    .insert(project)
+}
+
 function getResources(){
     return db('resources')
 }
 
-function getTasks(){
-    return db('tasks')
+function getResourceById(id) {
+    return db('users')
+      .where({ id })
+      .first();
+  }
+
+function insertResource(resource){
+    return db('resources')
+    .insert(resource)
 }
 
-// function (){
-//     return
-// }
+function getTasks(){
+    return db('tasks as t')
+    .select(
+        'project',
+        'p.description',
+        't.description as Task',
+        'notes',
+        't.completed',
+    )
+    .join('projects as p', 'p.id', 't.project_id')
+}
+
+function insertTask(task){
+    return db('tasks')
+    .insert(task)
+}
 
 // function (){
 //     return
--- projects/project-router.js
+++ projects/project-router.js
@@ -2,10 +2,80 @@ const router = require('express').Router()
 
 const Projects = require('./project-model')
 
+//projects endpoints
+
 router.get('/', (req, res) => {
     Projects.getProjects()
     .then(projects => {
         res.status(200).json({ data: projects })
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message:'Internal Server Error'})
+    })
+})
+
+router.post('/', (req, res) => {
+    const project = req.body
+
+    Projects.insertProject(project)
+    .then(() => {
+        res.status(200).json({ message: "project created"})
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message:'Internal Server Error'})
+    })
+})
+
+//resources endpoints
+
+router.get('/resources', (req, res) => {
+    Projects.getResources()
+    .then(resources => {
+        res.status(200).json({ data: resources })
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message:'Internal Server Error'})
+    })
+})
+
+router.post('/resources', (req, res) => {
+    const resource = req.body
+
+    Projects.insertResource(resource)
+    .then(() => {
+        res.status(200).json({ message: "resource added"})
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message:'Internal Server Error'})
+    })
+})
+
+//tasks endpoints
+
+router.get('/tasks', (req, res) => {
+    Projects.getTasks()
+    .then(resources => {
+        res.status(200).json({ data: resources })
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message:'Internal Server Error'})
+    })
+})
+
+router.post('/:id/tasks', (req, res) => {
+    const { description, notes } = req.body
+    const project_id = req.params.id
+
+    console.log(req.body)
+    console.log(project_id)
+    Projects.insertTask({project_id, description, notes})
+    .then(() => {
+        res.status(200).json({ message: "task added"})
     })
     .catch(error => {
         console.log(error)
